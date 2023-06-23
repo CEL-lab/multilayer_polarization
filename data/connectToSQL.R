a064734697dd589a5d615886a23ee6b49938a6dd
yusuf8834 <56593381+yusuf8834@users.noreply.github.com>
2023-06-23 18:49:25 +0300

    reciprocity

--- data/connectToSQL.R
+++ data/connectToSQL.R
@@ -3,14 +3,13 @@ library(DBI)
 library(RSQLite)
 
 # Connect to the SQLite database
-conn <- dbConnect(SQLite(), "csvyerine.db")
+conn <- dbConnect(SQLite(), "data/csvyerine.db")
 
 # List the tables
 tables <- dbListTables(conn)
-print(tables)
+tables
 
 # Read the table
 df <- dbReadTable(conn, "mentionMatrixAll")
-
 # Don't forget to close the connection
 dbDisconnect(conn)
