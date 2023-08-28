83fe83ff5d075e703da8439bdffb44a8b64baf1d
harunpirim <43437789+harunpirim@users.noreply.github.com>
2023-08-27 22:44:02 -0500

    republican and democratic subgraphs

---
+++ analysis/republican2.R
@@ -0,0 +1,73 @@
+#multiplex analysis of democratic affiliation only
+#required libraries
+library(DBI)
+library(RSQLite)
+library(igraph)
+library(multinet)#multiplex analysis
+library(ggplot2)
+library(ggraph)
+library(devtools)
+library(tidyverse)
+library(RColorBrewer)
+#devtools::install_github("manlius/muxViz")
+library(muxViz)#multiplex visualization
+# Connect to the SQLite database
+conn <- dbConnect(SQLite(), "/Users/harunpirim/Documents/GitHub/multilayer_polarization/data/csvyerine2.db")
+#read meta table
+metadata <- dbReadTable(conn, "metadata")
+# List all tables
+tables <- dbListTables(conn)
+# List supervised tables
+sup_tables <- tables[tables %>% str_detect("^sup")]
+#List unsupervised tables
+unsup_tables <- tables[tables %>% str_detect("^unsup")]
+#nodes that have democrat label
+repub_labels <- which(metadata['finalParty']=='Republican')
+#construct layers from tables from adjacency matrices
+construct_layers_repub <- function(type = "sup") {
+  layers <- list()  # Create an empty list to store data frames and matrices
+  for (i in 1:length(get(paste0(type, "_tables")))) {
+    df <- dbReadTable(conn, get(paste0(type, "_tables"))[i])
+    df <- df[, -1]  # Remove the first column
+    df <- df %>%
+      rename_with(~gsub("^X", "", .), starts_with("X")) %>% slice(which(metadata['finalParty']=='Republican')) %>%  select(which(metadata['finalParty']=='Republican')) %>% 
+      as.matrix()
+    # Assign the result to a list element
+    layers[[paste0("layer", i)]] <- df
+  }
+  return(layers)
+}
+#construct supervised layers
+layers_sup_repub <- construct_layers_repub("sup")
+#layers_sup is a list of adjacency matrices
+ilayers_sup_repub <- lapply(layers_sup_repub, function(adj_matrix) {
+  graph_from_adjacency_matrix(adj_matrix, mode = "undirected")
+})
+#construct unsupervised layers
+layers_unsup_repub <- construct_layers_repub("unsup")
+ilayers_unsup_repub <- lapply(layers_unsup_repub, function(adj_matrix) {
+  graph_from_adjacency_matrix(adj_matrix, mode = "undirected")
+})
+#create empty multiplex for supervised
+multiplex_sup_repub <- ml_empty()
+#add igraph layers
+for(i in 1:length(ilayers_sup_repub)){
+  if(length(E(ilayers_sup_repub[[i]])!=0))
+  add_igraph_layer_ml(multiplex_sup_repub,ilayers_sup_repub[[i]],sup_tables[i])
+}
+
+#create empty multiplex for unsupervised
+multiplex_unsup_repub <- ml_empty()
+#add igraph layers
+for(i in 1:length(ilayers_unsup_repub)){
+  if(length(E(ilayers_unsup_repub[[i]])!=0))
+  add_igraph_layer_ml(multiplex_unsup_repub,ilayers_unsup_repub[[i]],unsup_tables[i])
+}
+#plot
+plot(multiplex_sup_repub, vertex.labels = repub_labels, vertex.labels.cex = 0.3, vertex.color = 'red', vertex.size = 0.05)
+dev.off()
+plot(multiplex_unsup, vertex.labels = repub_labels, vertex.labels.cex = 0.3, vertex.color = 'red', vertex.size = 0.05)
+dev.off()
+#summary statistics for layers
+summary(multiplex_sup)
+summary(multiplex_unsup)
